12c796671b0afb849c139a2852109a2d8c8c0eb8
Baqar Ali <sayedalintl@gmail.com>
2024-03-18 00:53:53 +0530

    update

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-const url = "https://fakestoreapi.com/products";
+const url = "https://dummyjson.com/products";
 const cardsLists = document.getElementById("cards-lists");
 const numofcarts = document.querySelector(".no.ofcarts");
 const carts = document.getElementById("carts");
-const categories = document.querySelectorAll(".category");
+
+
 let str = "";
 let cart = [];
 let datas = undefined;
 let cartAddedvalue = false;
 let addCarts = [];
 let index = 0;
+let allcategories;
+
+String.prototype.capitalize = function(){
+  return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase()
+}
 
 //
 const showLists = async () => {
   try {
     const response = await fetch(url);
     const result = await response.json();
-    datas = result;
+    const {products} = result; 
+    datas = products.map(ele=>({...ele,price:Number(ele.price) * 82,category:ele.category.capitalize()}));
+    allcategories = [...new Set(datas.map(ele=>ele.category))]
+    
+    showCategory()
     iterateLists(datas);
   } catch (error) {
     console.error(error);
@@ -27,15 +37,17 @@ function iterateLists(data) {
   str = "";
   if (data.length > 0) {
     for (val of data) {
-      const { id, title, price, description, image, category } = val;
+      const { id, title, price, description, images, category } = val;
       str += `
-    <div class="card mx-4 my-3" style="width: 18rem;">
-    <img src=${image} class="card-img-top" alt="...">
+    <div class="card px-2 py-1 mx-4 my-3" style="width: 18rem;">
+    <img src=${images[0]} class="card-img-top" alt="...">
+    <i class="fa-solid fa-arrow-right"></i>
+    <i class="fa-solid fa-arrow-left"></i>
     <div class="card-body">
       <h5 class="card-title">${title}</h5>
-      <h6 class="card-price">$${price}</h6>
+      <h6 class="card-price">Rs ${price}</h6>
       <h6 class="card-category">${category}</h6>
-      <p class="card-text">$${description.substring(0, 100)}</p>
+      <p class="card-text">${description.substring(0, 100)}</p>
       <button onclick="onADD(${id})" class="addcart btn btn-primary">
       ${cartAddedvalue ? "Remove Item" : "AddtoCart"}</button>
       </div>
@@ -50,7 +62,6 @@ function iterateLists(data) {
 showLists();
 
 function onADD(i) {
-  gotoCart = true;
   if (cartAddedvalue) {
     index--;
       if (index) {
@@ -81,25 +92,68 @@ function onADD(i) {
   }
 }
 function showCart() {
+  filterprice.style.display = "none";
+  filterpriceValue.style.display = "none";
   cartAddedvalue = true;
   iterateLists(addCarts);
   let totalprice = 0;
   addCarts.forEach((cart) => {
     totalprice += cart.price;
   });
-  pricebox.innerHTML = `${cartAddedvalue && `<h1 id='prices'>Your price is $${totalprice}</h1>`
-    }`;
+  if(totalprice){
+    pricebox.innerHTML = `${cartAddedvalue && `<h1 id='prices'>Your price is $${totalprice}</h1>`
+      }`;
+  }
 }
 
-categories.forEach((category) => {
-  category.addEventListener("click", () => {
-    cartAddedvalue = false;
-    if (category.innerHTML == "Home") {
-      showLists();
-    } else {
-      let categoryid = category.innerHTML;
-      let newDataofLists = datas.filter((data) => data.category === categoryid);
-      iterateLists(newDataofLists);
-    }
-  });
-});
+
+let strforCategory = "";
+
+function showCategory(){
+  allcategories.forEach(ele=>{
+    strforCategory += `<li class="nav-item">
+    <a class="category cursor-pointer nav-link">${ele}</a>
+  </li>`;
+  categorybox.innerHTML = strforCategory;
+  })
+}
+
+categorybox.addEventListener('click',(e)=>{
+  cartAddedvalue = false;
+   if(e.target.classList.contains('category')){
+    const databyCategory = datas.filter(ele=>ele.category === e.target.innerHTML);
+    showFilteringPriceProgressBar()
+    iterateLists(databyCategory);
+    
+   }
+})
+
+
+
+
+const filterprice = document.querySelector('#filterprice');
+filterprice.min = Number(1000);
+filterprice.max = Number(200000);
+filterprice.value = Number(200000);
+const filterpriceValue = document.getElementById('filterpriceValue');
+filterpriceValue.innerHTML = `Items By Price ${filterprice.value}`;
+
+filterprice.addEventListener('change',(e)=>{
+  const targetprice = e.target.value;
+  filterpriceValue.innerHTML = `Items By Price ${filterprice.value}`;
+  let databyPrice = [...datas].filter(ele=>(Number(ele.price) <= targetprice));
+  iterateLists(databyPrice)
+  
+})
+
+
+home.addEventListener('click',()=>{
+  cartAddedvalue = false;
+  showFilteringPriceProgressBar()
+  iterateLists(datas);
+})
+
+function showFilteringPriceProgressBar(){
+  filterprice.style.display = "block";
+  filterpriceValue.style.display = "block";
+}
